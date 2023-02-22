9400be481c78650ca3ec78e380ac6eb11bb14602
Atapi <34801996+Sterophonick@users.noreply.github.com>
2023-02-22 16:00:34 -0700

    i stared at ghidra long enough lmao

--- sound.iwram.c
+++ sound.iwram.c
@@ -4,8 +4,9 @@ void ham_InitMixer(u16 bitrate)
 {
     undefined8 uVar1;
 
-    ham_mixer._24_4_ = 0; //what the fuck?
-    ham_mixer._20_4_ = 0;
+    //reset samples
+    ham_mixer.first_sample = 0;
+    ham_mixer.last_sample = 0;
 
     ham_mixer.bitrate = bitrate;
 
